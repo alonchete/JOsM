6f122266d9286370a4fbdf8835916af5c4b395f4
alonchete <alonsete.tyr@gmail.com>
2023-10-23 19:38:24 +0200

    nav2

--- JOsM/src/app/app.module.ts
+++ JOsM/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {NavComponent} from './components/nav/nav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     NavComponent,
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
