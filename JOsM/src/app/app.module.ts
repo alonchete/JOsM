fac2fc6f55e7e941d163496d5fbcbb18fbabcd5a
alonchete <alonsete.tyr@gmail.com>
2023-10-28 19:34:48 +0200

    inicio

--- JOsM/src/app/app.module.ts
+++ JOsM/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import {NavComponent} from './components/nav/nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { TargetsComponent } from './components/targets/targets.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -11,9 +13,12 @@ import { HeaderComponent } from './components/header/header.component';
     NavComponent,
     AppComponent,
     HeaderComponent,
+    CardsComponent,
+    TargetsComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- JOsM/src/app/components/nav/nav.component.ts
+++ JOsM/src/app/components/nav/nav.component.ts
@@ -9,9 +9,51 @@ import { Component } from "@angular/core";
 
 export class NavComponent{
 
-    
+    menuOptions = [
+        { id: 'opcion1', label: 'Inicio' },
+        {
+          id: 'opcion2',
+          label: 'Mecenazgo/Patrocinio',
+          subOptions: [
+            { label: 'Sociedades y empresas', link: '' },
+            { label: 'Personas y particulares', link: '' }
+          ]
+        },
+        { id: 'opcion3', label: 'Conciertos' },
+        {
+          id: 'opcion4',
+          label: 'Pruebas de acceso',
+          subOptions: [
+            { label: 'Pruebas de acceso 2023', link: '' },
+            { label: 'Formulario de acceso 2024', link: '' }
+          ]
+        },
+        {
+          id: 'opcion5',
+          label: 'La orquesta',
+          subOptions: [
+            { label: 'Calendario', link: '' },
+            { label: 'Funcionamiento', link: '' },
+            { label: 'JOsM desde dentro', link: '' },
+            { label: 'Musicos', link: '' },
+            { label: 'Directiva JOsM', link: '' },
+            { label: 'Temporada', link: '' },
+            { label: 'Prensa', link: '' }
+          ]
+        },
+        {
+          id: 'opcion6',
+          label: 'Pruebas de acceso',
+          subOptions: [
+            { label: 'Pruebas de acceso 2023', link: '' },
+            { label: 'Formulario de acceso 2024', link: '' }
+          ]
+        },
+        { id: 'opcion7', label: 'Miembros' }
+      ];
     constructor(){
         console.log("ta cargando");
+        
     }
 
 }
